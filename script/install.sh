cf1cb087e509cb136e665112bac2b473f8c6d56e
Jan Ferko <julyloov@gmail.com>
2013-09-18 15:03:08 +0200

    Vim-ruby plugin added to installation script

--- script/install.sh
+++ script/install.sh
@@ -37,6 +37,29 @@ sh ./script/solarized.sh
 echo " + Solarized installed"
 echo ""
 
+if test $(which ruby)
+then
+  echo "Cloning vim-ruby..."
+  git clone git@github.com:vim-ruby/vim-ruby.git vim-ruby
+  echo ""
+  echo " + Vim-ruby repository was successfully cloned!"
+  echo ""
+
+  echo "Installing vim-ruby"
+  ruby vim-ruby/bin/vim-ruby-install.rb
+  echo ""
+  if [ $? != 0 ]
+  then
+    echo " + Vim-ruby plugin was installed"
+  else
+    echo " x Vim-ruby plugin was not installed"
+  fi
+  echo ""
+  rm -rf vim-ruby
+  echo " + Vim ruby repository was removed"
+  echo ""
+fi
+
 echo "Good Job! Your vimdotfiles are set now"
 
 
