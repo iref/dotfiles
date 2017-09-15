d82a918c99cf8f9fc9e8ce7124bbf409fdbdbc9a
Jan Ferko <julyloov@gmail.com>
2017-09-15 10:53:27 +0200

    Switch install script to vim-plug
    Vundle is no longer maintained, so I use vim-plug as vim package manager

--- script/install.sh
+++ script/install.sh
@@ -4,6 +4,7 @@
 echo "Installing gitconfig..."
 echo ""
 cp gitconfig $HOME/.gitconfig
+cp git_templates $HOME/.git_templates
 echo ""
 echo " + gitconfig installed."
 
@@ -11,22 +12,27 @@ echo "Copying vimrc..."
 echo ""
 
 cp vimrc $HOME/.vimrc
+cp vimrc_background $HOME/.vimrc_background
+mkdir -p $HOME/.config/nvim
+ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
 
 echo " + vimrc copied."
 echo ""
 
-echo "Installing Vundle..."
+echo "Installing vim-plug..."
 echo ""
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "  + Vundle installed"
+
+echo "  + vim-plug installed"
 echo ""
 
 echo "Installing vim plugins..."
 echo ""
 
-vim +PluginInstall +qall
+vim +PlugInstall +qall
 
 echo " + Vim plugins installed"
 
