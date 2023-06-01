b7504acb2923159647b07a6bbaafbcf8f17990e1
Jan Ferko <julyloov@gmail.com>
2023-06-01 23:43:39 +0200

    Update asdf script

--- script/asdf.sh
+++ script/asdf.sh
@@ -1,9 +1,18 @@
 #!/bin/zsh
 
-asdf plugin-add clojure 
-asdf plugin-add maven
-asdf plugin-add sbt 
+echo "Install asdf"
+echo ""
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.3
+
+echo " + asdf successfully installed"
+echo ""
+
+asdf plugin-add java
 asdf plugin-add elixir 
 asdf plugin-add python 
 asdf plugin-add nodejs
-asdf plugin-add yarn
+
+asdf install python 3.11.3
+asdf install java adoptopenjdk-19.0.2+7
+asdf install nodejs 20.2.0
+asdf install elixir 1.14.5
